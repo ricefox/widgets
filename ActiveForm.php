3b5e7125ac7078d2220d2ed3ec0c9d3359edf738
tiehuoban <tiehuoban@163.com>
2016-03-24 11:39:00 +0800

    wrote a readme file

---
+++ ActiveForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wen
+ * Date: 2016/3/24
+ * Time: 11:20
+ */
+
+namespace ricefox\widgets;
+
+class ActiveForm
+{
+    public static function hello()
+    {
+        echo 'hello';
+    }
+}
